aa088956d4e2aae75bdfb7843e13d3862bb8a383
metlis <andrei.rusov8@gmail.com>
2018-12-02 01:00:07 +0300

    webpack added

---
+++ open_data_app/client/src/index.js
@@ -0,0 +1,32 @@
+function addParam(param) {
+    if (window.location.href.indexOf(param) > -1) return;
+    if (window.location.href.indexOf('?') > 0) {
+        var parsedData = /(.*)\=(.*)/.exec(param);
+        var parsedParam = parsedData[1];
+        var parsedValue = parsedData[2];
+        updateURLParameter(window.location.href, parsedParam, parsedValue)
+    } else {
+        window.location.href += '?' + param;
+    }
+}
+
+function updateURLParameter(url, param, paramVal) {
+    var newAdditionalURL = "";
+    var tempArray = url.split("?");
+    var baseURL = tempArray[0];
+    var additionalURL = tempArray[1];
+    var temp = "";
+    if (additionalURL) {
+        tempArray = additionalURL.split("&");
+        for (var i = 0; i < tempArray.length; i++) {
+            if (tempArray[i].split('=')[0] != param) {
+                newAdditionalURL += temp + tempArray[i];
+                temp = "&";
+            }
+        }
+    }
+
+    var rows_txt = temp + "" + param + "=" + paramVal;
+    url = baseURL + "?" + newAdditionalURL + rows_txt;
+    window.location.href = url.replace(/(.*)(page\=\d+&)(.*)/, '$1$3');
+}
--- settings.py
+++ settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'mathfilters',
     'open_data_app',
 ]
