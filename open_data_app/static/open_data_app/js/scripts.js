25eeb66cd4eb841aa0cc87e96ec06544050c49c6
metlis <andrei.rusov8@gmail.com>
2019-01-02 21:17:41 +0300

    dropdown menu

--- open_data_app/static/open_data_app/js/scripts.js
+++ open_data_app/static/open_data_app/js/scripts.js
@@ -30,3 +30,19 @@ function updateURLParameter(url, param, paramVal) {
     url = baseURL + "?" + newAdditionalURL + rows_txt;
     window.location.href = url.replace(/(.*)(page\=\d+&)(.*)/, '$1$3');
 }
+
+$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+  if (!$(this).next().hasClass('show')) {
+    $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+  }
+  var $subMenu = $(this).next(".dropdown-menu");
+  $subMenu.toggleClass('show');
+
+
+  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+    $('.dropdown-submenu .show').removeClass("show");
+  });
+
+
+  return false;
+});
