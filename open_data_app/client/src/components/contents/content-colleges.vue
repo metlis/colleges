80ee1103203c4771560b2b4e1fba8777361d82a8
metlis <andrei.rusov8@gmail.com>
2019-10-09 15:24:52 +0300

    colleges content separated to a separate component

---
+++ open_data_app/client/src/components/contents/content-colleges.vue
@@ -0,0 +1,152 @@
+<template>
+  <v-row>
+    <v-col cols="4" v-for="college in localColleges" :key="college.id">
+      <v-card>
+        <v-card-title style="word-break: normal !important">{{college.name}}</v-card-title>
+        <v-card-text>
+          <span @click="openCollegeUrl(college.url)" style="cursor: pointer">
+            {{college.url}}
+          </span>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn icon>
+            <v-icon color="amber" @click="removeCollege(college.id)">mdi-heart</v-icon>
+          </v-btn>
+          <v-btn outlined text @click="openCollegePage(college.id, college.slug)">
+            Page
+          </v-btn>
+          <div class="flex-grow-1"></div>
+          <v-btn
+            icon
+            @click="handleChevronClick(college.id)"
+          >
+            <v-icon>
+              {{ cardsStates[college.id] ? 'mdi-chevron-up' : 'mdi-chevron-down' }}
+            </v-icon>
+          </v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+          <div v-show="cardsStates[college.id]">
+            <v-card-text>
+              <v-divider />
+              <div>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <span :class="$style.container" v-on="on"><v-icon>mdi-map-marker
+                    </v-icon></span>
+                  </template>
+                  <span>Location</span>
+                </v-tooltip>
+                <span :class="$style.description"> {{college.city}}, {{college.state__name}}
+                </span><br>
+              </div>
+              <div v-if="college.carnegie__description">
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <span :class="$style.container" v-on="on"><v-icon>mdi-city
+                    </v-icon></span>
+                  </template>
+                  <span>Locale</span>
+                </v-tooltip>
+                <span :class="$style.description"> {{college.locale__description}}</span>
+                <br>
+              </div>
+              <div v-if="college.undergrad_students">
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <span :class="$style.container" v-on="on"><v-icon>mdi-account-multiple
+                    </v-icon></span>
+                  </template>
+                  <span>Undergraduate students</span>
+                </v-tooltip>
+                <span :class="$style.description"> {{college.undergrad_students}}</span>
+                <br>
+              </div>
+              <div v-if="college.religion__name">
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <span :class="$style.container" v-on="on"><v-icon>mdi-church
+                    </v-icon></span>
+                  </template>
+                  <span>Religious affiliation</span>
+                </v-tooltip>
+                <span :class="$style.description"> {{college.religion__name}}</span><br>
+              </div>
+              <div v-if="college.ownership__description">
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <span :class="$style.container" v-on="on"><v-icon>mdi-cash
+                    </v-icon></span>
+                  </template>
+                  <span>Ownership</span>
+                </v-tooltip>
+                <span :class="$style.description"> {{college.ownership__description}}</span>
+                <br>
+              </div>
+            </v-card-text>
+          </div>
+        </v-expand-transition>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'content-colleges',
+  props: ['colleges'],
+  data() {
+    return {
+      localColleges: [],
+      cardsStates: {},
+    };
+  },
+  methods: {
+    removeCollege(id) {
+      fetch(`/api/modify_favourites/?college_id=${id}`)
+        .then(response => response.text())
+        .then((data) => {
+          if (data === 'Removed') {
+            this.localColleges.forEach((col, index, obj) => {
+              if (col.id === id) {
+                obj.splice(index, 1);
+                const favBadge = document.getElementById('favourite-badge');
+                favBadge.innerText = String(Number(favBadge.innerText) - 1);
+              }
+            });
+          }
+        }).catch((err) => {
+          console.error(err);
+        });
+    },
+    openCollegePage(id, slug) {
+      const url = `/institution/${id}/${slug}`;
+      window.open(url, '_self');
+    },
+    openCollegeUrl(url) {
+      window.open(`//${url}`, '_self');
+    },
+    handleChevronClick(id) {
+      if (!this.cardsStates[id]) {
+        this.$set(this.cardsStates, id, true);
+      } else {
+        this.cardsStates[id] = !this.cardsStates[id];
+      }
+    },
+  },
+  created() {
+    this.localColleges = this.colleges;
+  },
+};
+</script>
+
+<style lang="stylus" module>
+  .container
+    display inline-block
+    width 25px
+    float left
+    margin-right 5px
+  .description
+    display block
+    overflow hidden
+</style>
--- open_data_app/client/src/layouts/layout-content.vue
+++ open_data_app/client/src/layouts/layout-content.vue
@@ -6,96 +6,7 @@
           <menu-left />
         </v-col>
         <v-col cols="7">
-          <v-row>
-            <v-col cols="4" v-for="college in localColleges" :key="college.id">
-              <v-card>
-                <v-card-title style="word-break: normal !important">{{college.name}}</v-card-title>
-                <v-card-text>
-                  <span @click="openCollegeUrl(college.url)" style="cursor: pointer">
-                    {{college.url}}
-                  </span>
-                </v-card-text>
-                <v-card-actions>
-                  <v-btn icon>
-                    <v-icon color="amber" @click="removeCollege(college.id)">mdi-heart</v-icon>
-                  </v-btn>
-                  <v-btn outlined text @click="openCollegePage(college.id, college.slug)">
-                    Page
-                  </v-btn>
-                  <div class="flex-grow-1"></div>
-                  <v-btn
-                    icon
-                    @click="handleChevronClick(college.id)"
-                  >
-                    <v-icon>
-                      {{ cardsStates[college.id] ? 'mdi-chevron-up' : 'mdi-chevron-down' }}
-                    </v-icon>
-                  </v-btn>
-                </v-card-actions>
-                <v-expand-transition>
-                  <div v-show="cardsStates[college.id]">
-                    <v-card-text>
-                      <v-divider />
-                      <div>
-                        <v-tooltip bottom>
-                          <template v-slot:activator="{ on }">
-                            <span :class="$style.container" v-on="on"><v-icon>mdi-map-marker
-                            </v-icon></span>
-                          </template>
-                          <span>Location</span>
-                        </v-tooltip>
-                        <span :class="$style.description"> {{college.city}}, {{college.state__name}}
-                        </span><br>
-                      </div>
-                      <div v-if="college.carnegie__description">
-                        <v-tooltip bottom>
-                          <template v-slot:activator="{ on }">
-                            <span :class="$style.container" v-on="on"><v-icon>mdi-city
-                            </v-icon></span>
-                          </template>
-                          <span>Locale</span>
-                        </v-tooltip>
-                        <span :class="$style.description"> {{college.locale__description}}</span>
-                        <br>
-                      </div>
-                      <div v-if="college.undergrad_students">
-                        <v-tooltip bottom>
-                          <template v-slot:activator="{ on }">
-                            <span :class="$style.container" v-on="on"><v-icon>mdi-account-multiple
-                            </v-icon></span>
-                          </template>
-                          <span>Undergraduate students</span>
-                        </v-tooltip>
-                        <span :class="$style.description"> {{college.undergrad_students}}</span>
-                        <br>
-                      </div>
-                      <div v-if="college.religion__name">
-                        <v-tooltip bottom>
-                          <template v-slot:activator="{ on }">
-                            <span :class="$style.container" v-on="on"><v-icon>mdi-church
-                            </v-icon></span>
-                          </template>
-                          <span>Religious affiliation</span>
-                        </v-tooltip>
-                        <span :class="$style.description"> {{college.religion__name}}</span><br>
-                      </div>
-                      <div v-if="college.ownership__description">
-                        <v-tooltip bottom>
-                          <template v-slot:activator="{ on }">
-                            <span :class="$style.container" v-on="on"><v-icon>mdi-cash
-                            </v-icon></span>
-                          </template>
-                          <span>Ownership</span>
-                        </v-tooltip>
-                        <span :class="$style.description"> {{college.ownership__description}}</span>
-                        <br>
-                      </div>
-                    </v-card-text>
-                  </div>
-                </v-expand-transition>
-              </v-card>
-            </v-col>
-          </v-row>
+          <content-colleges :colleges="colleges" />
         </v-col>
         <v-col cols="2" offset="1" style="padding: 0px">
           <menu-right-colleges />
@@ -108,63 +19,14 @@
 <script>
 import MenuLeft from '../components/menus/menu-left.vue';
 import MenuRightColleges from '../components/menus/menu-right-colleges.vue';
+import ContentColleges from '../components/contents/content-colleges.vue';
 
 export default {
   name: 'content-layout',
-  components: { MenuLeft, MenuRightColleges },
+  components: { MenuLeft, MenuRightColleges, ContentColleges },
   props: ['colleges'],
-  data() {
-    return {
-      localColleges: [],
-      cardsStates: {},
-    };
-  },
-  methods: {
-    removeCollege(id) {
-      fetch(`/api/modify_favourites/?college_id=${id}`)
-        .then(response => response.text())
-        .then((data) => {
-          if (data === 'Removed') {
-            this.localColleges.forEach((col, index, obj) => {
-              if (col.id === id) {
-                obj.splice(index, 1);
-                const favBadge = document.getElementById('favourite-badge');
-                favBadge.innerText = String(Number(favBadge.innerText) - 1);
-              }
-            });
-          }
-        }).catch((err) => {
-          console.error(err);
-        });
-    },
-    openCollegePage(id, slug) {
-      const url = `/institution/${id}/${slug}`;
-      window.open(url, '_self');
-    },
-    openCollegeUrl(url) {
-      window.open(`//${url}`, '_self');
-    },
-    handleChevronClick(id) {
-      if (!this.cardsStates[id]) {
-        this.$set(this.cardsStates, id, true);
-      } else {
-        this.cardsStates[id] = !this.cardsStates[id];
-      }
-    },
-  },
-  created() {
-    this.localColleges = this.colleges;
-  },
 };
 </script>
 
-<style lang="stylus" module>
-  .container
-    display inline-block
-    width 25px
-    float left
-    margin-right 5px
-  .description
-    display block
-    overflow hidden
+<style scoped>
 </style>
