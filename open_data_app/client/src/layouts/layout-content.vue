70fed41fc8519a847f42cb45ef321562de39de43
metlis <andrei.rusov8@gmail.com>
2019-10-22 15:05:18 +0300

    layout data refactored

--- open_data_app/client/src/layouts/layout-content.vue
+++ open_data_app/client/src/layouts/layout-content.vue
@@ -25,11 +25,11 @@
             ref="colleges"
             v-if="activeNavButton === 'Colleges'"
             :colleges="colleges"
-            :activeSortButton="activeSortButton"
-            :prevSortButton="prevSortButton"
-            :checkboxFilters="checkboxFilters"
-            :statesFilters="statesFilters"
-            :rangeFilters="rangeFilters"
+            :activeSortButton="contentColleges.activeSortButton"
+            :prevSortButton="contentColleges.prevSortButton"
+            :checkboxFilters="contentColleges.checkboxFilters"
+            :statesFilters="contentColleges.statesFilters"
+            :rangeFilters="contentColleges.rangeFilters"
           />
         </v-container>
       </v-content>
@@ -45,10 +45,10 @@
       <menu-right-colleges
         v-if="activeNavButton === 'Colleges'"
         :colleges="colleges"
-        @sortClick="changeSortButton"
-        @checkboxFilterChanged="updateCheckboxFilters"
-        @statesFilterChanged="updateStatesFilters"
-        @rangeFilterChanged="updateRangeFilters"
+        @sortClick="changeCollegesSortButton"
+        @checkboxFilterChanged="updateCollegesCheckboxFilters"
+        @statesFilterChanged="updateCollegesStatesFilters"
+        @rangeFilterChanged="updateCollegesRangeFilters"
         @reset="resetFilters"
       />
     </v-col>
@@ -117,11 +117,6 @@ export default {
   data() {
     return {
       activeNavButton: 'Colleges',
-      activeSortButton: '',
-      prevSortButton: '',
-      checkboxFilters: '',
-      statesFilters: '',
-      rangeFilters: '',
       classes: {
         leftMenu: 'd-none d-md-block',
         rightMenu: 'd-none d-md-block',
@@ -129,43 +124,54 @@ export default {
       mobileMenu: false,
       fab: false,
       fabClose: false,
+      contentColleges: {
+        activeSortButton: '',
+        prevSortButton: '',
+        checkboxFilters: '',
+        statesFilters: '',
+        rangeFilters: '',
+      },
     };
   },
   methods: {
     changeNavButton(val) {
       this.activeNavButton = val;
     },
-    changeSortButton(val) {
-      if (this.activeSortButton) this.prevSortButton = this.activeSortButton;
-      this.activeSortButton = val;
+    changeCollegesSortButton(val) {
+      if (this.contentColleges.activeSortButton) {
+        this.contentColleges.prevSortButton = this.contentColleges.activeSortButton;
+      }
+      this.contentColleges.activeSortButton = val;
       setTimeout(() => {
         this.$root.$emit('sort-click');
       }, 0);
     },
-    updateCheckboxFilters(val) {
-      this.checkboxFilters = val;
+    updateCollegesCheckboxFilters(val) {
+      this.contentColleges.checkboxFilters = val;
       setTimeout(() => {
         this.$root.$emit('checkbox-click');
       }, 0);
     },
-    updateStatesFilters(val) {
-      this.statesFilters = val;
+    updateCollegesStatesFilters(val) {
+      this.contentColleges.statesFilters = val;
       setTimeout(() => {
         this.$root.$emit('state-click');
       }, 0);
     },
-    updateRangeFilters(val) {
-      this.rangeFilters = val;
+    updateCollegesRangeFilters(val) {
+      this.contentColleges.rangeFilters = val;
       setTimeout(() => {
         this.$root.$emit('range-input');
       }, 0);
     },
     resetFilters() {
-      this.activeSortButton = '';
-      this.prevSortButton = '';
-      this.checkboxFilters = '';
-      this.statesFilters = '';
-      this.rangeFilters = '';
+      this.contentColleges = Object.assign({}, this.contentColleges, {
+        activeSortButton: '',
+        prevSortButton: '',
+        checkboxFilters: '',
+        statesFilters: '',
+        rangeFilters: '',
+      });
       goTo(this.$refs.top);
       setTimeout(() => {
         this.$root.$emit('reset-filters');
