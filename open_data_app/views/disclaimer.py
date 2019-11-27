438e1c805876756c6b88f65e6cd72e80c7eb732c
metlis <andrei.rusov8@gmail.com>
2019-11-27 23:16:06 +0300

    favourite colleges added

--- open_data_app/views/disclaimer.py
+++ open_data_app/views/disclaimer.py
@@ -7,8 +7,14 @@ def disclaimer(request):
     if 'cookie_agreement' in request.session:
         cookie_agreement = True
 
+    # ids of favourite colleges
+    favourite_colleges = []
+    if 'favourite_colleges' in request.session:
+        favourite_colleges = request.session['favourite_colleges']
+
     return render(request, 'disclaimer.html', {
         'domain': DOMAIN,
         'cookie_agreement': cookie_agreement,
         'version': STATIC_VERSION,
+        'favourite_colleges': favourite_colleges,
     })
